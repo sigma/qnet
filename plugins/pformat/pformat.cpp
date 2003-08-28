7c2c798c67919ec16c5ba12f4c91e0e3c78197af
Yann Hodique <yann.hodique@gmail.com>
2003-08-28 07:51:53 +0000

    pformat plugin 'append()' method implemented examples in README

--- plugins/pformat/pformat.cpp
+++ plugins/pformat/pformat.cpp
@@ -11,6 +11,8 @@
 //
 //
 
+#include <qregexp.h>
+
 #include "pformat.h"
 
 PLUGIN_FACTORY(PFormat,"pformat");
@@ -25,5 +27,18 @@ bool PFormat::isSlave() {
     return false;
 }
 
-void PFormat::append(const QString& msg) {}
+void PFormat::append(const QString& msg) {
+    QRegExp re("(\\w+)<-\"(.*)\"((,\\(\".*\":-?\\d+\\))*)");
+    QRegExp re2("\\(\"(.*)\":(-?\\d+)\\)");
+    if(re.exactMatch(msg)) {
+        QString value(re.cap(2));
+        QStringList args = QStringList::split(',',re.cap(3));
+        for(QStringList::Iterator it = args.begin(); it != args.end(); ++it) {
+            if(re2.exactMatch(*it))
+                value = value.arg(re2.cap(1),re2.cap(2).toInt());
+        }
+        getMaster()->context()->setVar(re.cap(1),value);
+        getMaster()->displayStderr("Setting " + re.cap(1) + " to " + value);
+    }
+}
 
