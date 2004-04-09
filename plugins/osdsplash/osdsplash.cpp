74f53b88a36b80e33ea8b81f0358e1e324be3f5b
Yann Hodique <yann.hodique@gmail.com>
2004-04-09 06:49:12 +0000

    new plugin : osd display

---
+++ plugins/osdsplash/osdsplash.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *   Copyright (C) 2002 by Yann Hodique                                    *
+ *   Yann.Hodique@lifl.fr                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include <qapplication.h>
+#include <qpainter.h>
+#include <qpixmap.h>
+#include <qcolor.h>
+#include <qregexp.h>
+#include <qpoint.h>
+
+#include "osdsplash.h"
+
+PLUGIN_FACTORY(OsdSplash,"splash");
+
+OsdSplash::OsdSplash(QWidget * /*parent*/, const char *name, Master * session)
+    : Page(0, name, session) {
+}
+
+OsdSplash::~OsdSplash() {}
+
+void OsdSplash::append(const QString & msg) {
+
+    QRegExp re("\\[([NS]?)([EO]?)\\](.*)");
+    if(re.exactMatch(msg)) {
+        QRect scr = QApplication::desktop()->screenGeometry();
+
+        QPoint pt = scr.center();
+
+        if(re.cap(1) == "S") {
+            pt.setY(scr.height() - rect().height());
+        } else if(re.cap(1) == "N") {
+            pt.setY(0);
+        } else {
+            pt.setY(pt.y() - rect().height()/2);
+        }
+
+        if(re.cap(2) == "E") {
+            pt.setX(scr.width() - rect().width());
+        } else if(re.cap(2) == "O") {
+            pt.setX(0);
+        } else {
+            pt.setX(pt.x() - rect().width()/2);
+        }
+
+        move(pt);
+        show();
+        repaint();
+    }
+}
---
+++ plugins/osdsplash/osdsplash.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *   Copyright (C) 2002 by Yann Hodique                                    *
+ *   Yann.Hodique@lifl.fr                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#ifndef _OSDSPLASH_H_
+#define _OSDSPLASH_H_
+
+#include <qpixmap.h>
+#include <qwidget.h>
+#include <qtimer.h>
+#include <qlabel.h>
+
+#include "master.h"
+#include "page.h"
+
+class OsdSplash : public Page {
+    Q_OBJECT
+    
+public:
+    OsdSplash(QWidget *parent, const char *name, Master * session);
+    ~OsdSplash();
+    
+    bool isSlave() {return false;}
+    
+public slots:
+    void append(const QString & msg);
+};
+
+#endif
